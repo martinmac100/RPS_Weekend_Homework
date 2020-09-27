181f091c7b5e478b561d19cba2ceef2d3f11e4fb
matinmac100 <matinmac100@yahoo.co.uk>
2020-09-27 09:47:59 +0100

    ensured the imports look correct

--- app/controller.py
+++ app/controller.py
@@ -1,5 +1,8 @@
 from app import app
+from flask import render_template
+from app.models.game_format import *
+from app.models.game import *
 
 @app.route('/')
-def test_message():
-    return "Hello"
+def index():
+    return render_template('index.html', title='Home')
