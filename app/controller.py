079cc0d7bb1ac6abd0353c4e84511a98a0bdbf1c
matinmac100 <matinmac100@yahoo.co.uk>
2020-09-27 22:16:35 +0100

    logic not working - had enough

--- app/controller.py
+++ app/controller.py
@@ -1,8 +1,19 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 from app.models.rps import *
 from app.models.player import *
 
 @app.route('/')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/get_result', methods=['POST', 'GET'])
+def get_result():
+    player_1_name = request.form.get ('player_1_name')
+    player_1_choice = request.form.get ('player_1_choice')
+    player_1 = Player(player_1_name, player_1_choice)
+    player_2_name = request.form.get ('player_2_name')
+    player_2_choice = request.form.get ('player_2_choice')
+    player_2 = Player(player_2_name, player_2_choice)
+
+    return render_template (index.html, player_1_name, player_1_choice, player_2_name, player_2_choice )
