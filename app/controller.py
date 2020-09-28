5f89d7951df16c5cb596fea9053b6f54d2679a30
matinmac100 <matinmac100@yahoo.co.uk>
2020-09-28 22:36:58 +0100

    updated logic etc

--- app/controller.py
+++ app/controller.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request, redirect
 from app.models.rps import *
 from app.models.player import *
 
@@ -7,13 +7,19 @@ from app.models.player import *
 def index():
     return render_template('index.html', title='Home')
 
-@app.route('/get_result', methods=['POST', 'GET'])
-def get_result():
-    player_1_name = request.form.get ('player_1_name')
-    player_1_choice = request.form.get ('player_1_choice')
+
+@app.route('/play-game', methods=['GET','POST'])
+def play_game():
+    
+    player_1_name = request.form('player_1_name')
+    player_1_choice = request.form('player_1_choice')
+    
     player_1 = Player(player_1_name, player_1_choice)
-    player_2_name = request.form.get ('player_2_name')
-    player_2_choice = request.form.get ('player_2_choice')
+    
+    player_2_name = request.form('player_2_name')
+    player_2_choice = request.form('player_2_choice')
+    
     player_2 = Player(player_2_name, player_2_choice)
 
-    return render_template ('index.html', 'player_1_name', 'player_1_choice', 'player_2_name', 'player_2_choice' )
+    
+    return redirect ('/play-game')
